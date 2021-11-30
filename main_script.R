66f0e0f97f90440ff388bc1840396d8dcd62b664
“Ale”-cianfa <alessandra.cianfanelli@gmail.com>
2021-11-30 14:28:42 +0000

    ale commiting a map

--- main_script.R
+++ main_script.R
@@ -7,6 +7,8 @@ library(viridis) #for the colors
 library(RColorBrewer) #for some more colors
 library(maps) #for the base map data
 library(hrbrthemes) #for the fonts in ggplot
+library("wesanderson")
+
 
 #LOADING THE DATASETS ----
 
@@ -31,7 +33,7 @@ colnames(flood) <- c("Country", "Start", "End", "Deaths",
 str(flood)
 
 ## Turning all variables into integers/numbers----
-flood$Country <- as.factor(flood$Country)
+flood$Country <- as.character(flood$Country)
 flood$Start <- as.integer(flood$Start)
 flood$End <- as.integer(flood$End)
 flood$Deaths <- as.integer(gsub(",", "", flood$Deaths))
@@ -55,7 +57,7 @@ unique(flood$Start)
   #Indonesia, Thailand, Vietnam, Malaysia, Philippines, Cambodia
 #TITLE: 35 years of flooding in South-East Asia, the economic and human cost 
   
-SEA <- c("Indonesia","Thailand", "Southern Thailand", "Thailand -  Typhoons Ira and Lola",
+SEA1 <- c("Indonesia","Thailand", "Southern Thailand", "Thailand -  Typhoons Ira and Lola",
          "Thailand - Typhoon Gay", "Thailand - Typhoon Harry", 
          "Thailand - Typhoons Gloria and Frankie", "Vietnam", "Vietnam - Cyclone Cecil", 
          "Vietnam - Tropical Storm Chip", "Vietnam - Typhoon Fritz", "Vietnam - Typhoon Linda",
@@ -75,7 +77,7 @@ SEA <- c("Indonesia","Thailand", "Southern Thailand", "Thailand -  Typhoons Ira 
 #NB: this one doesn't work "Philippines - Tropical Storm Flo / Kadiang"
 
 flood_sea <- flood %>%
-  filter(Country %in% SEA) %>%
+  filter(Country %in% SEA1) %>%
   mutate(Country = str_replace(Country, "Southern Thailand", "Thailand")) %>% 
   mutate(Country = str_replace(Country, "Thailand -  Typhoons Ira and Lola", "Thailand")) %>% 
   mutate(Country = str_replace(Country, "Thailand - Typhoon Gay", "Thailand")) %>% 
@@ -106,25 +108,74 @@ flood_sea <- flood %>%
   mutate(Country = str_replace(Country, "Philippines - Typhoons Gloring (Gloria) and Huaning (Herb)", "Philippines")) %>% 
   mutate(Country = str_replace(Country, "Philippines -Typhoon Nell", "Philippines"))
 
-
 ## Calculating the mean over the years----
+flood_sea <- flood_sea %>%
+  mutate(Country = str_replace(Country, "Philippines - Typhoons Gloring (Gloria) and Huaning (Herb)", "Philippines"))
+  #this doesn't work and idk why 
 
-flood_sea2 <-flood_sea %>% 
+sea_pop <-flood_sea %>% 
   group_by(Country) %>%
   summarize(Mean_affected_pop = mean(Affected_population, na.rm=TRUE)) %>% 
   ungroup()
 
-flood_sea3 <-flood_sea %>% 
+sea_pop <- sea_pop[-c(5, 6, 7, 8), ]
+
+sea_pop$Mean_affected_pop <- sea_pop$Mean_affected_pop / 100000 #in 100.000 thousands
+
+
+sea_damage <-flood_sea %>% 
   group_by(Country) %>% 
-  summarize(Mean_damage = mean(Adjusted_damage_usd, na.rm=TRUE)) %>% 
+  summarize(Mean_damage = mean(Adjusted_damage_usd, na.rm=TRUE)) %>%
   ungroup()
+
+sea_damage <- sea_damage[-c(5, 6, 7, 8), ]
+
+sea_damage$Mean_damage <- sea_damage$Mean_damage / 1000000 #making the numbers in millions 
+
+sea_damage$Country
 
 # Staring to map it----
 
-  
-  
+Sea_2 <- c("Cambodia", "Indonesia", "Malaysia",
+           "Philippines", "Thailand", "Vietnam")
+
+sea_map <- world %>% 
+  filter(region %in% Sea_2) %>% 
+  dplyr::select(-subregion) 
+
+Sea_flood_p <- left_join(sea_map, sea_pop, by = c("region" = "Country")) 
+
+str(Sea_flood_p)
+
+scales::show_col(c("#9DBF9E", "#A84268", "#FCB97D", "#C0BCB5", "#4A6C6F", "#FF5E5B"))
+
+(Sea_flood_pmap <- ggplot() +
+    geom_polygon(data = Sea_flood_p, aes(x = long, y = lat, group = group, fill = Mean_affected_pop) 
+                 , color="black", size = 0.1) + #plot the data points on the map
+    theme_void() + #choosing what type of background we want to display 
+    scale_fill_gradientn(colors = c("#9DBF9E", "#FCB97D", "#A84268"), na.value = "grey80")+
+    theme(text = element_text(family = "Futura-Bold"),
+          legend.position = c(0.87, 0.75),
+          legend.title = element_text(family = "Futura-Bold", size = 10),
+          legend.text = element_text(family = "Futura-Medium", size = 10)) +
+    labs(y = "Latitude", x = "Longitude", #labs can be used to rename the axis and titles of your plots
+         fill = "Affected population",
+         title = "Population affected by flooding in South East Asia from 1980 to 2014"))
+
+# LINK INSERT MAP: https://upgo.lab.mcgill.ca/2019/12/13/making-beautiful-maps/
 
 
+
+Sea_flood_d <- left_join(sea_map, sea_damage, by = c("region" = "Country")) 
+
+str(Sea_flood_d)
+
+(Sea_flood_dmap <- ggplot() +
+    geom_polygon(data = Sea_flood_d, aes(x = long, y = lat, group = group, fill = Mean_damage) 
+                 , color="black", size = 0.3) + #plot the data points on the map
+    theme_minimal() + #choosing what type of background we want to display 
+    scale_fill_viridis(direction = -1) +
+    coord_map())  #making the projection nice
 
   
 
